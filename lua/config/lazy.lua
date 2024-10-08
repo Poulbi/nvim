80ac911de548e2b2799a4957dbd00bf15dc552b8
Raymaekers Luca <luca@keyfried.com>
2024-10-08 16:05:22 +0200

    checkpoint

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -50,11 +50,11 @@ require("lazy").setup({
 	{
 		"ray-x/lsp_signature.nvim",
 		event = "VeryLazy",
-		opts = {
-			doc_lines = 0,
-		},
-		config = function(_, opts)
-			require("lsp_signature").setup(opts)
+		config = function()
+			require("lsp_signature").setup({
+				doc_lines = 0,
+				hint_enable = false,
+			})
 		end,
 	},
 	{
