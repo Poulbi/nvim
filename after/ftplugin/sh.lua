1e0a20bdee561c2ca7df5299f0826cd3d7b4d02f
Raymaekers Luca <luca@keyfried.com>
2024-10-20 19:41:44 +0200

    checkpoint

--- after/ftplugin/sh.lua
+++ after/ftplugin/sh.lua
@@ -2,4 +2,4 @@ local map = vim.keymap.set
 
 map("n", ",v", [[<cmd>s/\(\s*\)\(.\+\)/\1"$(\2)"/<cr>]], { desc = "Surround with variable" })
 map("n", ",p", [[<cmd>s/\(\s*\)\(.\+\)/\1>\&2 printf '\2: %s\\n' "$\2"/<cr>]], { desc = "Surround var with printf" })
-map("n", ",e", [[<cmd>s/\(\s*\)\(.\+\)/\1[ "$\2" ] || exit 1'/<cr>]], { desc = "Surround var with exit if empty" })
+map("n", ",e", [[<cmd>s/\(\s*\)\(.\+\)/\1[ "$\2" ] || exit 1/<cr>]], { desc = "Surround var with exit if empty" })
