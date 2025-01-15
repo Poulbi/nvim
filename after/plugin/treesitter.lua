56fe071c7a7e1c42f520e4490c500f76562f8072
Raymaekers Luca <luca@keyfried.com>
2025-01-15 23:43:24 +0100

    checkpoint

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -10,6 +10,7 @@ require("nvim-treesitter.configs").setup({
 	highlight = {
 		enable = true,
 		additional_vim_regex_highlighting = false,
+		disable = { "c" },
 	},
 	incremental_selection = {
 		enable = true,
