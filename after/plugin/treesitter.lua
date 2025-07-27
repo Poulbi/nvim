d931307a650fa97a8ad49b8cf71717f055776d8e
Raymaekers Luca <luca@keyfried.com>
2025-07-27 21:48:13 +0200

    checkpoint

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -3,10 +3,6 @@ require("nvim-treesitter.configs").setup({
 	sync_install = false,
 	auto_install = true,
 
-	indent = {
-		enable = true,
-	},
-
 	highlight = {
 		enable = true,
 		additional_vim_regex_highlighting = false,
