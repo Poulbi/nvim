0ead40c86e60b0b8f937f2aa2cb7ea71fb9ef377
Raymaekers Luca <luca@keyfried.com>
2025-10-10 14:14:36 +0200

    checkpoint

--- lua/config/map.lua
+++ lua/config/map.lua
@@ -14,8 +14,6 @@ local function verbose_toggle(value, name)
 	return toggle
 end
 
-
-
 -- open config
 map("n", " c", function()
 	vim.cmd("cd ~/.config/nvim")
@@ -30,7 +28,7 @@ map("i", "<C-e>", "<C-o>A", { noremap = true })
 map("n", " q", "<cmd>q!<cr>", { noremap = true })
 map("n", " Q", "<cmd>qa!<cr>", { noremap = true })
 map("n", "!", "<cmd>sp<CR><cmd>term<CR>", { desc = "Open terminal" })
-map("n", "<M-b>", "<cmd>make<cr>", { desc = "Make" })
+map("n", "<M-b>", "<cmd>write<cr><cmd>make<cr>", { desc = "Make" })
 
 -- better indenting
 map("v", "<", "<gv")
@@ -58,10 +56,14 @@ vim.api.nvim_create_user_command("Hide", function()
 end, {})
 
 vim.api.nvim_create_user_command("ThisDir", function()
-      vim.api.nvim_paste([[
+	vim.api.nvim_paste(
+		[[
 ThisDir="$(dirname "$(readlink -f "$0")")"
 cd "$ThisDir"
-  ]], false, -1)
+  ]],
+		false,
+		-1
+	)
 end, {})
 
 map("n", " x", "!cx %", { desc = "Toggle file as executable" })
@@ -84,6 +86,24 @@ end, { desc = "Toggle diagnostics" })
 -- editing
 map("n", " sf", ":%s/\\<<C-R><C-W>\\>//gc<Left><Left><Left>", { desc = "Replace word under cursor in file" })
 map("n", " sl", ":s/\\<<C-R><C-W>\\>//g<Left><Left>", { desc = "Replace word under cursor on current line" })
+
+-- From https://github.com/neovim/neovim/discussions/35081
+map("v", " sf", function()
+ local s_start = vim.fn.getpos "."
+   local s_end = vim.fn.getpos "v"
+   local lines = vim.fn.getregion(s_start,s_end)
+ local selection = lines
+   local text = vim.fn.escape(selection[1], [[\/]])
+   for i = 2, #selection do
+      text = text .. '\\n' .. vim.fn.escape(selection[i], [[\/]])
+   end
+   local clear_selection =
+      vim.api.nvim_replace_termcodes("<C-u>", true, false, true)
+   local double_left =
+      vim.api.nvim_replace_termcodes("<Left><Left>", true, false, true)
+   local keys_to_feed = ":" .. clear_selection .. "%s/" .. text .. "//gc" .. double_left
+   vim.fn.feedkeys(keys_to_feed)
+end, { desc = "Replace selected text through file" })
 
 map("n", "]q", "<cmd>cn<cr>", { desc = "Next item in quickfix", silent = true })
 map("n", "[q", "<cmd>cp<cr>", { desc = "Next item in quickfix", silent = true })
